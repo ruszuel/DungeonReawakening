723fabbb8a04c0d878708dc7cb4f0524523e6739
jeffreyzamora <153573212+jeffreyzamora@users.noreply.github.com>
2024-05-26 19:09:49 +0800

    movements ng character jeff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,69 +1,122 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-//[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class PlayerController : MonoBehaviour
 {
-    public float jumpImpulse = 8f;
+    public float jumpImpulse = 12f;
     public float walkSpeed = 4f;
     public float airWalkSpeed = 4f;
     public float runSpeed = 7f;
-    Vector2 moveInput;
-    //TouchingDirections touchingDirections;
+    private Vector2 moveInput;
+    private TouchingDirections touchingDirections;
+
+    public float CurrentMoveSpeed
+    {
+        get
+        {
+            if (CanMove)
+            {
+                if (IsMoving && !touchingDirections.IsOnWall)
+                {
+                    if (touchingDirections.IsGrounded)
+                    {
+                        return IsRunning ? runSpeed : walkSpeed;
+                    }
+                    else
+                    {
+                        return airWalkSpeed;
+                    }
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
 
     [SerializeField]
     private bool _isMoving = false;
 
+    public bool IsMoving
+    {
+        get { return _isMoving; }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool(AnimationStrings.isMoving, value);
+        }
+    }
+
     [SerializeField]
     private bool _isRunning = false;
 
-    public bool _isFacingRight = true;
+    public bool IsRunning
+    {
+        get { return _isRunning; }
+        set
+        {
+            _isRunning = value;
+            animator.SetBool(AnimationStrings.isRunning, value);
+        }
+    }
 
-    Rigidbody2D rb;
-    Animator animator;
+    private bool _isFacingRight = true;
 
-    public bool IsMoving { get; private set; }
+    public bool IsFacingRight
+    {
+        get { return _isFacingRight; }
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+            }
+
+            _isFacingRight = value;
+        }
+    }
+
+    public bool CanMove
+    {
+        get
+        {
+            return animator.GetBool(AnimationStrings.canMove);
+        }
+    }
+
+    private Rigidbody2D rb;
+    private Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        //touchingDirections = GetComponent<TouchingDirections>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        touchingDirections = GetComponent<TouchingDirections>();
     }
 
     private void FixedUpdate()
     {
-        //rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
-        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
-
-       // animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
+        animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-
         IsMoving = moveInput != Vector2.zero;
-
-        //SetFacingDirection(moveInput);
+        SetFacingDirection(moveInput);
     }
 
-    /*private void SetFacingDirection(Vector2 moveInput)
+    private void SetFacingDirection(Vector2 moveInput)
     {
         if (moveInput.x > 0 && !IsFacingRight)
         {
@@ -73,5 +126,34 @@ public class PlayerController : MonoBehaviour
         {
             IsFacingRight = false;
         }
-    }*/
+    }
+
+    public void onRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            IsRunning = true;
+        }
+        else if (context.canceled)
+        {
+            IsRunning = false;
+        }
+    }
+
+    public void onJump(InputAction.CallbackContext context)
+    {
+        if (context.started && touchingDirections.IsGrounded && CanMove)
+        {
+            animator.SetTrigger(AnimationStrings.jumpTrigger);
+            rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
+        }
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            animator.SetTrigger(AnimationStrings.attackTrigger);
+        }
+    }
 }
