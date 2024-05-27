419bba46e2129ed2f75a8136b6f5e3a7e5d2b077
jeffreyzamora <153573212+jeffreyzamora@users.noreply.github.com>
2024-05-27 15:34:35 +0800

    character na namamatay

---
+++ Assets/Damageable.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    Animator animator;
+
+    [SerializeField]
+    private int _maxHealth = 100;
+
+    public int MaxHealth
+    {
+        get
+        {
+            return _maxHealth;
+        }
+        set
+        {
+            _maxHealth = value;
+        }
+    }
+
+    [SerializeField]
+    private int _health = 100;
+
+    public int Health
+    {
+        get
+        {
+            return _health;
+        }
+        set
+        {
+            _health = value;
+
+            if (_health < 0)
+            {
+                IsAlive = false;
+            }
+        }
+    }
+
+    [SerializeField]
+    private bool _isAlive = true;
+
+    [SerializeField]
+    private bool isInvinsible = false;
+    private float timeSinceHit = 0f;
+    public float invisibilityTime = 0.25f;
+
+    public bool IsAlive { 
+        get
+        {
+            return _isAlive;
+        }
+        set
+        {
+            _isAlive = value;
+            animator.SetBool(AnimationStrings.isAlive, value);
+            Debug.Log("IsAlive set" + value);
+        }
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        if (isInvinsible) 
+        {
+            if(timeSinceHit > invisibilityTime)
+            {
+                isInvinsible = false;
+                timeSinceHit = 0;
+            }
+
+            timeSinceHit += Time.deltaTime;
+        }
+        Hit(10);
+    }
+
+    private void Hit (int damage)
+    {
+        if(IsAlive && !isInvinsible)
+        {
+            Health -= damage;
+            isInvinsible=true;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -106,6 +106,14 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public bool IsAlive
+    {
+        get
+        {
+            return animator.GetBool(AnimationStrings.isAlive);
+        }
+    }
+
     Rigidbody2D rb;
     Animator animator;
 
@@ -139,9 +147,17 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = context.ReadValue<Vector2>();
 
-        IsMoving = moveInput != Vector2.zero;
+        if (IsAlive)
+        {
+            IsMoving = moveInput != Vector2.zero;
 
-        SetFacingDirection(moveInput);
+            SetFacingDirection(moveInput);
+        }
+        else
+        {
+            IsMoving = false;
+        }
+        
     }
 
     private void SetFacingDirection(Vector2 moveInput)
